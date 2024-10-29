b1d1e1c490b88dae284f9f69606bcd6472d0889a
Hampus Nordin <nordin.hampus@gmail.com>
2024-10-29 17:34:45 +0100

    Added support for path-variant as an alternative to nodeId-variant in shared lib: sitevisionApi

--- src/_lib/shared/sitevision-api.js
+++ src/_lib/shared/sitevision-api.js
@@ -6,13 +6,19 @@ export const ONLINE = 1;
  * 
  * @param {object} options An object literal with options.
  * @param {string} options.nodeId The node identifier to perform the api request on.
+ * @param {string} options.path The path to perform the api request on.
+ * @param {string} options.siteName The site's name. Required when using path.
  * @param {number} [options.version=ONLINE] The version to use.
  * @param {string} [options.apiMethod='nodes'] The API method to call. (Usually one of: 'nodes', 'properties', 'contentNodes', 'headless')
  * @param {object} [options.options={}] Options to be passed to the API method.
  * @param {string} [options.origin=window.location.origin] The origin to send the request to.
  * @returns {Response}
  */
-export default async function sitevisionApi ({ nodeId, version = ONLINE, apiMethod = 'nodes', options = {}, origin = window.location.origin }) {
+export default async function sitevisionApi ({ nodeId, path, siteName, version = ONLINE, apiMethod = 'nodes', options = {}, origin = window.location.origin }) {
+  if (!nodeId && !(path && siteName)) {
+    throw 'Api needs either a nodeId or a path.';
+  }
+  
   const apiParams = encodeURIComponent(JSON.stringify(options));
   const fetchOpts = {
     headers: {
@@ -22,7 +28,7 @@ export default async function sitevisionApi ({ nodeId, version = ONLINE, apiMeth
   };
 
   const v = [OFFLINE, ONLINE].includes(version) ? version : ONLINE;
-  const url = `${origin}/rest-api/1/${v}/${nodeId}/${apiMethod}?format=json&json=${apiParams}`;
+  const url = `${origin}/rest-api/1/${v}/${nodeId || (siteName + '/' + path.replace(/^\/|\/$/, ''))}/${apiMethod}?format=json&json=${apiParams}`;
 
   if (v === OFFLINE) {
     fetchOpts.credentials = 'same-origin';
