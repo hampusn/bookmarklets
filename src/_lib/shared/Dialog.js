8524a36ebb350b0d272cfb454309680a6d1a2143
Hampus Nordin <nordin.hampus@gmail.com>
2025-01-08 18:59:36 +0100

    Updated shared lib Dialog with new functionality :)

--- src/_lib/shared/Dialog.js
+++ src/_lib/shared/Dialog.js
@@ -1,3 +1,5 @@
+import Events from "./Events";
+
 const CLASS_BASE = 'env-modal-dialog';
 const SPINNER_CLASS = 'env-spinner-bounce';
 const DOCUMENT = window.document;
@@ -13,15 +15,19 @@ const ENVISION = window.envision;
  */
 
 export class Dialog {
-  constructor ({ dialogId, title, size }) {
+  constructor ({ dialogId, title, size, closeText = 'Close' }) {
     this.id = dialogId;
     this.isLoading = false;
+    this.isDisabled = false;
     this.title = title;
     this.size = size;
+    this.closeText = closeText;
+    this.context = {};
   
     this.tid = dialogId + '-t'; // title
     this.thid = dialogId + '-th'; // title holder
     this.cid = dialogId + '-c'; // content
+    this.eid = dialogId + '-e'; // error
     this.fid = dialogId + '-f'; // footer
     this.lid = dialogId + '-l'; // loader
   }
@@ -42,7 +48,7 @@ export class Dialog {
    * @param {(Function|String|HTMLElement)} initialContent 
    * @returns {Dialog} Returns self for chainability.
    */
-  init (initialContent) {
+  async init (initialContent) {
     const dialogId = this.id;
     const titleId = this.tid;
   
@@ -50,7 +56,13 @@ export class Dialog {
       return this;
     }
 
-    initialContent = typeof initialContent === 'function' ? initialContent.call(this) : initialContent;
+    let initialContentError;
+
+    try {
+      initialContent = typeof initialContent === 'function' ? await initialContent.call(null, this) : initialContent;
+    } catch (e) {
+      initialContentError = e;
+    }
 
     const contentString = typeof initialContent === 'string' ? initialContent : '';
     const sizeClass = this.size ? `${CLASS_BASE}__dialog--${this.size}` : '';
@@ -70,20 +82,41 @@ export class Dialog {
           </header>
           <div class="${CLASS_BASE}__body">
             <div id="${this.cid}">${contentString}</div>
+            <div id="${this.eid}" class="env-alert env-alert--danger" hidden>
+              <span data-error-holder></span>
+              <button type="button" class="env-alert__close env-button env-button--link env-button--icon env-button--icon-small" data-dismiss="alert">
+                Close
+                <svg class="env-icon env-icon--xx-small">
+                  <use href="/sitevision/envision-icons.svg#icon-delete"></use>
+                </svg>
+              </button>
+            </div>
           </div>
           <footer id="${this.fid}" class="${CLASS_BASE}__footer env-d--flex env-flex--justify-content-between">
-            <button type="button" data-modal-dialog-dismiss class="env-button env-m-left--a">Close</button>
+            <button type="button" data-modal-dialog-dismiss class="env-button env-m-left--a">${this.closeText}</button>
           </footer>
         </section>
       </div>
     </div>`
     ));
 
+    if (initialContentError) {
+      this.renderError(initialContentError);
+    }
+
+    Events.onClick(this.el(this.eid).querySelector('[data-dismiss="alert"]'), () => {
+      this.renderError('');
+    });
+
     if (initialContent instanceof HTMLElement) {
       this.el(this.cid).appendChild(initialContent);
     }
 
     return this;
+  }
+
+  isOpen () {
+    return this.el(this.id)?.classList.contains('env-modal-dialog--show');
   }
 
   /**
@@ -108,38 +141,103 @@ export class Dialog {
     this.isLoading = !!isLoading;
     this.el(this.lid).classList.toggle(`${SPINNER_CLASS}--hide`, !this.isLoading);
   }
+
+  setDisabled (isDisabled) {
+    this.isDisabled = !!isDisabled;
+    this.el(this.id).inert = this.isDisabled;
+    this.el(this.cid).style.opacity = this.isDisabled ? 0.5 : 1;
+    this.el(this.fid).querySelectorAll('.env-button').forEach((btn) => btn.disabled = this.isDisabled);
+  }
   
   /**
    * @param {string} errorMessage 
    */
   renderError (errorMessage) {
-    this.updateContent(
-      `<div class="env-alert env-alert--danger">${errorMessage}</div>`
-    );
+    const hasError = !!errorMessage;
+    const errorElement = this.el(this.eid);
+
+    errorElement.querySelector('[data-error-holder]').innerHTML = errorMessage;
+    errorElement.hidden = !hasError;
+
+    this.el(this.cid).hidden = hasError;
+  }
+
+  onClose (callback) {
+    if (typeof callback === 'function') {
+      // TODO: should probably start using new Dialog api instead of old ModalDialog so we can 
+      // remove jQuery as a dependency.
+      jQuery('#' + this.id).on('hide.env-modal-dialog', (e) => {
+        callback.call(null, this, e);
+      });
+    }
+
+    return this;
+  }
+
+  onClosed (callback) {
+    if (typeof callback === 'function') {
+      // TODO: should probably start using new Dialog api instead of old ModalDialog so we can 
+      // remove jQuery as a dependency.
+      jQuery('#' + this.id).on('hidden.env-modal-dialog', (e) => {
+        callback.call(null, this, e);
+      });
+    }
+
+    return this;
+  }
+
+  hide (callback) {
+    ENVISION.dialog('#' + this.id, 'hide').then(callback ? callback.bind(null, this) : null);
+    return this;  
   }
 
   /**
-   * @param {Function} cb Callback run when Envision.dialog promise has finished.
+   * @param {Function} callback Callback run when Envision.dialog promise has finished.
    * @returns {Dialog}
    */
-  toggle (cb) {
-    ENVISION.dialog('#' + this.id, 'toggle').then(cb);
+  toggle (callback) {
+    ENVISION.dialog('#' + this.id, 'toggle').then(callback ? callback.bind(null, this) : null);
     return this;  
+  }
+
+  remove () {
+    this.el(this.id).remove();
   }
 };
 
 export default Dialog;
 
-export const getInstance = (function () {
-  const instances = {};
-  /**
-   * @returns {Dialog}
-   */
-  return function (dialogId, opts = {}) {
-    if (!instances[dialogId]) {
-      instances[dialogId] = new Dialog({ ...opts, dialogId });
-    }
+/**
+ * @returns {Record<string,Dialog>}
+ */
+export const getCache = (key = null) => {
+  const instances = window.__BookmarkletDialogInstances = window.__BookmarkletDialogInstances || {};
 
-    return instances[dialogId];
-  };
-})();
+  if (key) {
+    return instances[key];
+  }
+
+  return instances;
+};
+
+/**
+ * @returns {Dialog}
+ */
+export const getInstance = (dialogId, opts = {}) => {
+  const instances = getCache();
+
+  if (!instances[dialogId]) {
+    instances[dialogId] = new Dialog({ ...opts, dialogId });
+  }
+
+  return instances[dialogId];
+};
+
+export const removeInstance = (dialogId) => {
+  const instances = getCache();
+
+  if (instances[dialogId]) {
+    instances[dialogId].remove();
+    delete instances[dialogId];
+  }
+};
